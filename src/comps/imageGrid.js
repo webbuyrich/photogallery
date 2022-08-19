926844fee7c498a99a4cd49c5eb08d3217359d85
Richard Peterson <webbuyrich@gmail.com>
2022-08-19 15:57:09 -0500

    connected imageGrid to useFirestore

--- src/comps/imageGrid.js
+++ src/comps/imageGrid.js
@@ -1,6 +1,11 @@
 import React from "react";
+import useFirestore from "../hooks/useFirestore";
+
 
 const ImageGrid = () => {
+    const { docs } = useFirestore('images');
+    console.log(docs)
+
     return(
         <div className="img-grid">
             image
