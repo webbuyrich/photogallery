de0ed16058739312e85671b29b9ae553fbe1412f
Richard Peterson <webbuyrich@gmail.com>
2022-08-19 12:15:21 -0500

    show error if upload file type does not match type

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 
 const UploadForm = () =>{
     const[file, setFile] = useState(null)
+    const[error, setError] = useState(null);
+
 
     const types = ['image/png', 'image/jpeg', 'image/jpg']
 
@@ -16,12 +18,18 @@ const UploadForm = () =>{
         // if a file is selected, setFile and update the state
         if(selected && types.includes(selected.type)){
             setFile(selected)
+        } else {
+            setFile(null);
+            setError('Please select an image file (png, jpeg, or jpg)')
         }
     }
 
     return(
         <form>
             <input type="file" onChange={changeHandler}/>
+            <div className="output">
+                { error && <div className="error">{error}</div>}
+            </div>
         </form>
     )
 }
