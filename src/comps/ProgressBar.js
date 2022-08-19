832ddb854a14400637308c346d0efa0d79952515
Richard Peterson <webbuyrich@gmail.com>
2022-08-19 15:04:37 -0500

    remove progress bar after successful upload

--- src/comps/ProgressBar.js
+++ src/comps/ProgressBar.js
@@ -1,9 +1,15 @@
-import { React } from "react";
+import { React, useEffect } from "react";
 import  useStorage from "../hooks/useStorage";
 
 const ProgressBar = ({ file, setFile }) => {
     const { url, progress } = useStorage(file);
-    console.log(progress,url)
+    console.log(progress, url);
+
+    useEffect( () => {
+        if(url){
+            setFile(null)
+        }
+    }, [url, setFile])
 
     return(
         <div className="progress-bar" style={{ width: progress + '%'}}></div>
