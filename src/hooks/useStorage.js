c61968992de9b2114fba780488406367efaa6272
Richard Peterson <webbuyrich@gmail.com>
2022-08-19 13:50:02 -0500

    added explanation comments

--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -1,3 +1,4 @@
+import { async } from "@firebase/util";
 import { useState, useEffect } from "react";
 import { projectStorage } from "../firebase/config";
 
@@ -13,6 +14,28 @@ const useStorage = (file) =>{
 
     useEffect( ()=>{
         // references where file should be saved
-        const storageRef = projectStorage.ref(file.name)
+        const storageRef = projectStorage.ref(file.name);
+
+        //take a file and put it in the ref location. detect when progress or complete states change
+        storageRef.put(file).on('stage_changed', (snap) => {
+            // get progress and save to setProgress
+            let percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
+            setProgress(percentage);
+
+            // detect error and save to setError
+        }, (err) => {
+            setError(err)
+
+            // wait till image is uploaded and save to setUrl
+        }, async () => {
+            const url = await storageRef.getDownloadURL();
+            setUrl(url)
+        })
+
     },[file])
+
+    // values that will be available to access
+    return { progress, url, error }
 }
+
+export default useStorage;
