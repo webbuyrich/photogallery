9ae9ada9d00e4b1ac91bce810efbde99f4eff167
Richard Peterson <webbuyrich@gmail.com>
2022-08-19 12:08:57 -0500

    add image types that can be uploaded to conditional statement

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -4,14 +4,17 @@ import React, { useState } from "react";
 const UploadForm = () =>{
     const[file, setFile] = useState(null)
 
+    const types = ['image/png', 'image/jpeg', 'image/jpg']
+
     // access file user selected
     const changeHandler = (e) =>{
        
         // target first file only
         let selected = e.target.files[0];
+        console.log("selected information",selected)
         
         // if a file is selected, setFile and update the state
-        if(selected){
+        if(selected && types.includes(selected.type)){
             setFile(selected)
         }
     }
