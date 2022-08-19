12477a33697b665124388698f4c9877a94b61326
Richard Peterson <webbuyrich@gmail.com>
2022-08-19 13:52:58 -0500

    create ProgressBar component

---
+++ src/comps/ProgressBar.js
@@ -0,0 +1,10 @@
+import { React } from "react";
+import { useStorage } from "../hooks/useStorage";
+
+const ProgressBar = () => {
+    return(
+        <div className="progress-bar">progress</div>
+    )
+}
+
+export default ProgressBar
