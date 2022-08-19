0805c253d993e0206e60510e2126f60d9cc778b6
Richard Peterson <webbuyrich@gmail.com>
2022-08-19 14:14:22 -0500

    updated config to use /compat. created progressBar file. updated form file

--- src/comps/ProgressBar.js
+++ src/comps/ProgressBar.js
@@ -7,4 +7,4 @@ const ProgressBar = () => {
     )
 }
 
-export default ProgressBar
+export default ProgressBar;
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
-import * as firebase from "firebase/app";
-import 'firebase/storage'
-import 'firebase/firestore'
+import firebase from "firebase/compat/app";
+import 'firebase/compat/storage';
+import 'firebase/compat/firestore';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
