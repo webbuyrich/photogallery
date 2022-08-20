5bdab664c7be2a6487ddcce5949ec2b8887c3538
Richard Peterson <webbuyrich@gmail.com>
2022-08-20 00:14:03 -0500

    animate layout

--- src/comps/imageGrid.js
+++ src/comps/imageGrid.js
@@ -11,7 +11,8 @@ const ImageGrid = ({ setSelectedImg }) => {
     return(
         <div className="img-grid">
             { docs && docs.map( doc => (
-                <motion.div className="img-wrap" key={doc.id}                 
+                <motion.div className="img-wrap" key={doc.id}   
+                layout              
                 whileHover={{ opacity: 1}}
                 onClick={ () => setSelectedImg(doc.url) }
                 >
